42b10471e8c1358d6f7b1f56bc059f1fe2de2a94
Brandon Watts <brandonwatts@outlook.com>
2017-08-26 12:20:28 -0400

    Cont

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,16 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new webpack.ProvidePlugin({jQuery: 'jquery', $: 'jquery', jquery: 'jquery'}),
-    new webpack.ProvidePlugin({tether: 'tether', Tether: 'tether'})
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default'],
+        // In case you imported plugins individually, you must also require them here:
+        Util: "exports-loader?Util!bootstrap/js/dist/util",
+        Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      })
   ],
   devtool: '#source-map'
 }
